0c79b0a75eae45f889396252e0630f58227767b3
Rahul Chauhan <rahul.chauhan@sprinklr.com>
2025-09-17 17:15:18 +0530

    [chore] add baseweb design library and react-use for custom hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
+// libs
+import { useState } from "react";
+import useMount from "react-use/lib/useMount";
+import { Client as Styletron } from "styletron-engine-monolithic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { BaseProvider, DarkTheme } from "baseui";
+
+// styles
 import "@/styles/globals.css";
+
+// types
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [styletronEngine, setStyletronEngine] = useState<Styletron | null>(
+    null,
+  );
+
+  useMount(() => {
+    setStyletronEngine(new Styletron());
+  });
+
+  if (!styletronEngine) {
+    return null;
+  }
+
+  return (
+    <StyletronProvider value={styletronEngine}>
+      <BaseProvider theme={DarkTheme}>
+        <Component {...pageProps} />
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
